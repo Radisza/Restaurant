2d7dacc2c96ff4ec5bd76dd45d20ba69a2d6824a
Ela <radisza5@gmail.com>
2024-04-04 17:12:02 +0200

    Add basic source files and html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const { title } = require('process');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })
+  ]
+};
