4272bc6f7087d6df9f7f0b24c81871be86f54371
Ela <radisza5@gmail.com>
2024-04-04 17:30:52 +0200

    Format files using prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Restaurant
+
 https://www.theodinproject.com/lessons/node-path-javascript-webpack
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const { title } = require('process');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const { title } = require("process");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })
-  ]
+      template: "./src/index.html",
+    }),
+  ],
 };
